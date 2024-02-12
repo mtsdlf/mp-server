bea4ca30011dc53527c60d9e9912ee07807be808
mtsdlf <mts@gmail.com>
2024-02-12 04:38:14 -0300

    now main endpoint can receive an array

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express"
 import cors from "cors"
 
-import {MercadoPagoConfig, preference} from "mercadopago"
+import {MercadoPagoConfig, Preference} from "mercadopago"
 const client = new MercadoPagoConfig({
     accessToken: "TEST-5856636980528286-021121-8fc2acdb93fca144c793698e6023fd4c-524393398"
 })
 
 const app = express()
 const port = 3000
-
+console.log("start")
 app.use(cors())
 app.use(express.json());
 
@@ -18,15 +18,14 @@ app.get("/", (req, res) => {
 
 app.post("/create_preference", async (req, res) => {
     try {
+        console.log(req.body.products)
         const body = {
-            items: [
-                {
-                    title: req.body.title,
-                    quantity: Number(req.body.quantity),
-                    unit_price: Number(req.body.price),
-                    currency_id: "ARS"
-                },
-            ],
+            items: req.body.products.map(product => ({
+                title: product.title,
+                quantity: Number(product.quantity),
+                unit_price: Number(product.price),
+                currency_id: "ARS"
+            })),
             back_urls: {
                 success: "https://google.com",
                 failure: "https://google.com",
@@ -35,7 +34,8 @@ app.post("/create_preference", async (req, res) => {
             auto_return: "approved"
         }
 
-        const preference = new preference(client);
+        const preference = new Preference(client);
+        console.log(body)
         const result = await preference.create({ body })
 
         res.json({
